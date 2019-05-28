eb7e0121745af75f921f340ec1f7ff4b22ac8e72
Petya Buchkova <petya.buchkova@gmail.com>
2019-05-28 13:19:25 +0200

    Added Register and Login pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,38 +26,27 @@
     </v-navigation-drawer>
     <v-toolbar :clipped-left="clipped" fixed app>
       <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
+      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
+      </v-btn> -->
+      <!-- <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
+      </v-btn> -->
+      <!-- <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>remove</v-icon>
-      </v-btn>
+      </v-btn> -->
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+      <!-- <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>menu</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <navbar />
 
-    <!-- <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>-->
     <v-footer :fixed="fixed" app>
       <span>&copy; 2019</span>
     </v-footer>
@@ -65,12 +54,8 @@
 </template>
 
 <script>
-import Navbar from '~/components/Navbar.vue'
-
 export default {
-  components: {
-    Navbar
-  },
+  components: {},
   data() {
     return {
       clipped: false,
@@ -84,14 +69,14 @@ export default {
         },
         {
           icon: 'bubble_chart',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'Login',
+          to: '/login'
         }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Netbook'
     }
   }
 }
---
+++ pages/register.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+          <v-card class="elevation-12">
+            <v-toolbar dark color="primary">
+              <v-toolbar-title>Register</v-toolbar-title>
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+              <Notification v-if="error" :message="error" />
+
+              <v-form method="post" @submit.prevent="register">
+                <v-text-field
+                  v-model="name"
+                  prepend-icon="person"
+                  name="name"
+                  label="Name"
+                  type="text"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="username"
+                  prepend-icon="person"
+                  name="username"
+                  label="Username"
+                  type="text"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="email"
+                  prepend-icon="email"
+                  name="email"
+                  label="Email"
+                  type="email"
+                  class="input"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  id="password"
+                  v-model="password"
+                  prepend-icon="lock"
+                  name="password"
+                  label="Password"
+                  type="password"
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary">Register</v-btn>
+            </v-card-actions>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-card-text class="text-xs-right">
+                Already have an account?
+              </v-card-text>
+              <v-btn color="primary" flat nuxt to="/login">
+                Login
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+import Notification from '~/components/Notification'
+
+export default {
+  components: {
+    Notification
+  },
+
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+
+  methods: {
+    async register() {
+      try {
+        await this.$axios.post('register', {
+          username: this.username,
+          email: this.email,
+          password: this.password
+        })
+
+        await this.$auth.loginWith('local', {
+          data: {
+            email: this.email,
+            password: this.password
+          }
+        })
+
+        this.$router.push('/')
+      } catch (e) {
+        this.error = e.response.data.message
+      }
+    }
+  }
+}
+</script>
